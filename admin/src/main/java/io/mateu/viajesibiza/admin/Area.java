d27d5a495b74d2fd99b5f6dcfcc4d0c52dcfb763
miguelperezcolom@gmail.com <mgperez@quonext-tur.com>
2018-03-13 12:12:28 +0100

    aaa

--- admin/src/main/java/io/mateu/viajesibiza/admin/Area.java
+++ admin/src/main/java/io/mateu/viajesibiza/admin/Area.java
@@ -13,7 +13,7 @@ public class Area extends io.mateu.ui.core.client.app.AbstractArea {
 
     @Override
     public List<AbstractModule> getModules() {
-        List<AbstractModule> l = Lists.newArrayList(new Booking(), new Producto(), new Reports(), new Admin());
+        List<AbstractModule> l = Lists.newArrayList(new Confirm());
         if (MateuUI.hasPermission(1)) l = Lists.newArrayList(new Booking(), new Producto(), new Reports(), new Admin());
         return l;
     }
