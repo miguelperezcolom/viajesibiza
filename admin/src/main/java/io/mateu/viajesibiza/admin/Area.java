0619dc8a5bf40d5d34128a23d54f17654f0ae514
miguel <miguelperezcolom@gmail.com>
2018-04-03 20:40:10 +0200

    xx

--- admin/src/main/java/io/mateu/viajesibiza/admin/Area.java
+++ admin/src/main/java/io/mateu/viajesibiza/admin/Area.java
@@ -4,6 +4,7 @@ import com.google.common.collect.Lists;
 import io.mateu.ui.core.client.app.AbstractModule;
 import io.mateu.ui.core.client.app.MateuUI;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Area extends io.mateu.ui.core.client.app.AbstractArea {
@@ -12,9 +13,15 @@ public class Area extends io.mateu.ui.core.client.app.AbstractArea {
     }
 
     @Override
+    public List<AbstractModule> getModules() {
+        List<AbstractModule> l = super.getModules();
+        if (!MateuUI.hasPermission(1)) l = Lists.newArrayList(l.get(1));
+        return l;
+    }
+
+    @Override
     public List<AbstractModule> buildModules() {
-        List<AbstractModule> l = Lists.newArrayList(new Confirm());
-        if (MateuUI.hasPermission(1)) l = Lists.newArrayList(new Booking(), new Confirm(), new Producto(), new Reports(), new Admin());
+        List<AbstractModule> l = Lists.newArrayList(new Booking(), new Confirm(), new Producto(), new Reports(), new Admin());
         return l;
     }
 }
