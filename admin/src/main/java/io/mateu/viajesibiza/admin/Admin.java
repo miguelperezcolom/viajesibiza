8c7e346f741f87391f91a5c5c02ed528bd431c13
Miguel Pérez Colom <miguelperezcolom@gmail.com>
2018-04-16 21:31:37 +0200

    xx

--- admin/src/main/java/io/mateu/viajesibiza/admin/Admin.java
+++ admin/src/main/java/io/mateu/viajesibiza/admin/Admin.java
@@ -2,17 +2,13 @@ package io.mateu.viajesibiza.admin;
 
 import io.mateu.erp.model.authentication.User;
 import io.mateu.erp.model.config.AppConfig;
-import io.mateu.erp.model.financials.Actor;
-import io.mateu.erp.model.monitoring.Alarm;
-import io.mateu.erp.model.monitoring.Watchdog;
-import io.mateu.erp.model.monitoring.Watcher;
+import io.mateu.erp.model.partners.Actor;
 import io.mateu.erp.model.workflow.AbstractTask;
 import io.mateu.erp.model.world.City;
 import io.mateu.ui.core.client.app.AbstractModule;
 import io.mateu.ui.core.client.app.MateuUI;
 import io.mateu.ui.core.client.app.MenuEntry;
 import io.mateu.ui.mdd.client.MDDAction;
-import io.mateu.ui.mdd.client.MDDMenu;
 import io.mateu.ui.mdd.client.MDDOpenEditorAction;
 
 import java.util.ArrayList;
